d589094cd394eefef09d98d0271f45927dbd449a
harshal1711 <57212336+harshal1711@users.noreply.github.com>
2019-10-31 15:55:32 +0530

    Update signupvalidate.js

--- signupvalidate.js
+++ signupvalidate.js
@@ -16,47 +16,48 @@ function validateform()
   	if (email == "")             //if email field is empty,display alert
 	{
 	    alert("E-Mail must be filled out");
-	    name.focus();
+	    email.focus();
 	    return false;
   	}
 
   	if (phone == "")             //if phone no. field is empty,display alert
 	{
 	    alert("Phone no. must be filled out");
-	    name.focus();
+	    phone.focus();
 	    return false;
   	}
 
   	if (isNaN(phone) || (phone.length!=10))             //if phone no. field is non-numeric or length of phone no.string is not equal to 10 display alert
 	{
 	    alert("Invalid Type of Phone no.");
-	    name.focus();
+	    phone.focus();
 	    return false;
   	}
 
   	if (pwd == "")            						//If password field is empty, display alert
 	{
 	    alert("Password must be filled out");
-	    name.focus();
+	    pwd.focus();
 	    return false;
   	}
   	if (pwd.length<8)             					//If password length is less than 8, display alert
 	{
 	    alert("Password must be atleast 8 characters long.");
-	    name.focus();
+	    pwd.focus();
 	    return false;
   	}
   	if (pwdr == "")            						//If re-type password field is empty, display alert
 	{
 	    alert("Re-Type password for verification");
-	    name.focus();
+	    pwdr.focus();
 	    return false;
   	}
 
   	if (pwd != pwdr)            					//If the password doesnt match with the retyped one, display alert
 	{
 	    alert("Passwords must match");
-	    name.focus();
+	    pwd.focus();
+	    pwdr.focus();
 	    return false;
   	}
 
